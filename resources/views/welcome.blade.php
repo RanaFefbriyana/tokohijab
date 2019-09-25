da938ba711aa69e3251dcddc930725d931d23cab
Sofiwasilah <sofiwasilah99@gmail.com>
2019-09-25 14:15:43 +0700

    ubah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laracasts
+                    Laracastssopi
                 </div>
 
                 <div class="links">
